8955be3d680c93d644277cadcab224682baa3d80
nagnello <nagnello@L15202039987.wctc.msft>
2015-02-10 11:52:31 -0600

    Added Startup Class

---
+++ src/lab1/Startup.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab1;
+
+/**
+ *
+ * @author nagnello
+ */
+public class Startup {
+    
+    
+    public static void main(String[] args) {
+        ProgrammingCourse introCourse = new IntroToProgrammingCourse("Intro To Programming","101");
+        ProgrammingCourse javaCourse = new IntroToProgrammingCourse("Intro To Java","201");
+        ProgrammingCourse AdvancedCourse = new IntroToProgrammingCourse("Advanced Java","301");
+        
+        
+    }
+}

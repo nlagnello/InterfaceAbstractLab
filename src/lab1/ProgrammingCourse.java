0d9d515a7768946fa30114ae6b7f701d95150b2e
nagnello <nagnello@L11713042776.wctc.msft>
2015-02-10 15:35:11 -0600

    Added final to setcredits

--- src/lab1/ProgrammingCourse.java
+++ src/lab1/ProgrammingCourse.java
@@ -47,7 +47,7 @@ public abstract class ProgrammingCourse {
     }
        
     
-    public void setCredits(double credits) {
+    public final void setCredits(double credits) {
         if(credits < 0.5 || credits > 4.0) {
             JOptionPane.showMessageDialog(null,
                     "Error: credits must be in the range 0.5 to 4.0");
